925b5d54e00d8e7fa2c04b0a5b2f6af295025231
Kathleen-Hwang <sujeongkathleenhwang@gmail.com>
2016-06-11 00:25:36 +0900

    Update traverseVectorByRandomIterator.cpp
    add += operation

--- traverseVectorByRandomIterator.cpp
+++ traverseVectorByRandomIterator.cpp
@@ -8,10 +8,15 @@ int main() {
 	
 	v.push_back(10);
 	v.push_back(20);
+	v.push_back(30);
 
 	vector<int>::iterator it = v.begin();
 
 	cout << it[1] << endl;
+
+	it += 2;
+	
+	cout << *it << endl;
 
 	return 0;
 }
