fa687090d7cf78e5fc7b2918368aebcb9d3b240c
Kathleen-Hwang <sujeongkathleenhwang@gmail.com>
2016-06-11 00:37:55 +0900

    Create vectorWithFindAlgorithm.cpp

---
+++ vectorWithFindAlgorithm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	vector<int> v;
+	
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	
+	vector<int>::iterator it = find(v.begin(), v.end(), 30);
+	
+	if (it != v.end()) {
+		cout << *it << endl;
+	} else {
+		cout << "error : not found!" << endl;
+	}
+	
+	return 0;
+}
