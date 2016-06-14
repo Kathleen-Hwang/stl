5b4dd7a8fc4b805b84ae965006c1be6bd7e36f20
Kathleen-Hwang <sujeongkathleenhwang@gmail.com>
2016-06-14 20:41:30 +0900

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -10,15 +10,15 @@ int main() {
 
 	// output)
 	// size : 1, capacity : 1, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 2, capacity : 2, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 3, capacity : 4, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 4, capacity : 4, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 5, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 6, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 7, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 8, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
-  // size : 9, capacity : 16, maxsize : 4611686018427387903                                                                                                                                                                                                                           
-  // size : 10, capacity : 16, maxsize : 4611686018427387903
+    // size : 2, capacity : 2, maxsize : 4611686018427387903                                                                                                                                                                                                                            
+    // size : 3, capacity : 4, maxsize : 4611686018427387903                                                                                                                                                                                                                            
+    // size : 4, capacity : 4, maxsize : 4611686018427387903                                                                                                                                                                                                                            
+    // size : 5, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
+    // size : 6, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
+    // size : 7, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
+    // size : 8, capacity : 8, maxsize : 4611686018427387903                                                                                                                                                                                                                            
+    // size : 9, capacity : 16, maxsize : 4611686018427387903                                                                                                                                                                                                                           
+    // size : 10, capacity : 16, maxsize : 4611686018427387903
 	for (int i = 0; i < 10; i++) {
 	    v.push_back(i);
 	    cout << "size : " << v.size() << ", capacity : " << v.capacity() << ", maxsize : " << v.max_size() << endl;
@@ -29,9 +29,11 @@ int main() {
 		cout << v[i] << endl;
 	}
                                     // output)
-  cout << v.size() << endl;       // 10
-  cout << v.capacity() << endl;   // 16
-  cout << v.max_size() << endl;   // 4611686018427387903
-  
+    cout << v.size() << endl;       // 10
+    cout << v.capacity() << endl;   // 16
+    cout << v.max_size() << endl;   // 4611686018427387903
+    
+    
+    
 	return 0;
 }
